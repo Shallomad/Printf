e1d00bfb1c93776d8dd33870acd091bb12436cb9
Eznira <ihimarinze@gmail.com>
2023-07-23 15:27:47 -0700

    added printf

---
+++ printf.c
@@ -0,0 +1,70 @@
+#include "main.h"
+/**
+ * _printf - printf function that supports %c and %s conversions.
+ *
+ * @format: A format string containing zero or more conversion specifiers.
+ *          %c - Character
+ *          %s - String
+ *
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int count_char = 0;
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
+
+					_putchar(c);
+					count_char++;
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char*);
+
+					while (*s)
+					{
+						_putchar(*s);
+						s++;
+						count_char++;
+					}
+					break;
+				}
+				case '%':
+				{
+					_putchar('%');
+					count_char++;
+					break;
+				}
+				default:
+					_putchar('%');
+					count_char++;
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count_char++;
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (count_char);
+}
