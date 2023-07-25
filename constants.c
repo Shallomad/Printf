9be2b5b9dff8e92c8bc2d9605bc6788ca25f0f9c
Shallomad <Shallomadzo@outlook.com>
2023-07-25 13:24:42 -0700

    added file: constant.c

---
+++ constants.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+const placeholder_struct_t placeholder_map[] = {
+	{'c', print_char},
+	{'s', print_string},
+	{'%', print_percent},
+	{'d', print_int},
+	{'i', print_int},
+	{'b', print_bin},
+	{'u', print_unsigned_decimal},
+	{'o', print_octal},
+	{'x', _print_lower_hex},
+	{'X', _print_upper_hex},
+};
+
+const int placeholder_map_size = sizeof(placeholder_map)
+	/ sizeof(placeholder_map[0]);
